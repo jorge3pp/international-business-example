1d4a5a067b38ba05cf64979ee4b38ab879c9ca53
jpoveda <jpoveda@facephi.com>
2021-09-16 12:09:34 +0200

    FEAT: Created SwiftUI's project

---
+++ international-business-example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  international-business-example
+//
+//  Created by Jorge Poveda on 16/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ international-business-example/international_business_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  international_business_exampleApp.swift
+//  international-business-example
+//
+//  Created by Jorge Poveda on 16/9/21.
+//
+
+import SwiftUI
+
+@main
+struct international_business_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
