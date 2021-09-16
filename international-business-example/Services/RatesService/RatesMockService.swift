8920ad78d2a5d471e891cc5f6e52739387750f44
jpoveda <jpoveda@facephi.com>
2021-09-16 18:28:44 +0200

    FEAT: Created Void methods

---
+++ international-business-example/Services/RatesService/RatesMockService.swift
@@ -0,0 +1,19 @@
+//
+//  RatesMockService.swift
+//  international-business-example
+//
+//  Created by Jorge Poveda on 16/9/21.
+//
+
+import Foundation
+
+class RatesMockService: RatesServiceProtocol {
+    func getCurrencyRates() {
+        //let a = try await
+    }
+    
+    @asyncHandler internal func makeBrakefastAsync(){
+        
+    }
+
+}
---
+++ international-business-example/Services/RatesService/RatesServiceProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  RatesServiceProtocol.swift
+//  international-business-example
+//
+//  Created by Jorge Poveda on 16/9/21.
+//
+
+import Foundation
+
+protocol RatesServiceProtocol {
+    func getCurrencyRates()
+}
